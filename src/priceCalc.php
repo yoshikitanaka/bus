29efa7f93b221387f31f01b732efb21fb008389e
yoshiki_tanaka <yoshiki@iii-planning.com>
2015-06-22 16:03:47 +0900

    完成

--- src/priceCalc.php
+++ src/priceCalc.php
@@ -8,13 +8,25 @@
 namespace TripleI\bus;
 
 
-class priceCalc {
+/**
+ * http://nabetani.sakura.ne.jp/hena/ord9busfare/
+ * Class priceCalc
+ * @package TripleI\bus
+ */
+class priceCalc
+{
+
+    /**
+     * データ
+     * @var int
+     */
+    private $data;
 
     /**
      * 大人料金
      * @var int
      */
-    private $basic_fee;
+    private $base_fee;
 
     /**
      * 乗客
@@ -22,50 +34,130 @@ class priceCalc {
      */
     private $riders = [];
 
-    /**
-     * 大人・定期券あり
-     * @var array
-     */
-    private $Aps = [];
-
-    /**
-     * 子供・通常
-     * @var array
-     */
-    private $Cns = [];
-
-    /**
-     * 幼児・通常
-     * @var array
-     */
-    private $lns = [];
-
-    /**
-     * 幼児・福祉割引
-     * @var array
-     */
-    private $lws = [];
-
 
     public function dispatch($data)
     {
-
+        $this->parseData($data);
+        return $this->calc();
     }
 
 
     public function parseData($data)
     {
+        $this->data = $data;
         $ret = explode(':', $data);
-        $this->basic_fee = $ret[0];
+        $this->base_fee = $ret[0];
 
         $riders = $ret[1];
-        $this->riders = explode(',', $riders);
+        $riders = explode(',', $riders);
+        foreach ($riders as $rider) {
+            $this->sortToArray($rider);
+        }
+    }
 
+    private function sortToArray($rider)
+    {
+        $type = substr($rider, 0, 1);
+        $class = substr($rider, 1);
+        switch($type) {
+            case 'A':
+                $this->classToArray('A', $class);
+                break;
+            case 'C':
+                $this->classToArray('C', $class);
+                break;
+            case 'I':
+                $this->classToArray('I', $class);
+                break;
+        }
+    }
+
+    private function classToArray($type, $class)
+    {
+        switch($class) {
+            case 'n': // 通常 割引なし
+                $this->riders[$type]['n'][] = true;
+                break;
+            case 'w': // 福祉 1/2引き
+                $this->riders[$type]['w'][] = true;
+                break;
+            case 'p': // パス
+                $this->riders[$type]['p'][] = true;
+                break;
+        }
     }
 
 
-    public function getBasicFee()
+    public function calc()
     {
-        return $this->basic_fee;
+        $adult_count = 0;
+        $base_fee = $this->base_fee;
+        $base_fee_half = round($base_fee / 2, -1);
+        $base_fee_qtr = round($base_fee_half / 2, -1);
+
+        $fee = 0;
+
+        // 大人
+        $A = @$this->riders['A'];
+        if (@count($A['n']) > 0) {
+            $adult_count = count($A['n']) * 2;
+            $fee = $fee + $base_fee * count($A['n']);
+        }
+        if (@count($A['w']) > 0) {
+            $adult_count = $adult_count + count($A['w']) * 2;
+            $fee = $fee + $base_fee_half * count($A['w']);
+        }
+        if (@count($A['p']) > 0) {
+            $adult_count = $adult_count + count($A['p']) * 2;
+        }
+
+        // 子供
+        $C = @$this->riders['C'];
+        if (@count($C['n']) > 0) {
+            $fee = $fee + $base_fee_half * count($C['n']);
+        }
+        if (@count($C['w']) > 0) {
+            $fee = $fee + $base_fee_qtr * count($C['w']);
+        }
+
+        /**
+         * 幼児
+         * 幼児はadult_counterがあればカンターの数だけ積算しない
+         */
+        $I = @$this->riders['I'];
+        // 通常料金
+        if (@count($I['n']) > 0) {
+
+            while ($adult_count > 0 && count($I['n']) > 0) {
+                array_pop($I['n']);
+                $adult_count--;
+            }
+
+            $fee = $fee + $base_fee_half * count($I['n']);
+        }
+
+        // 福祉割引
+        if (@count($I['w']) > 0) {
+            while ($adult_count > 0 && count($I['w']) > 0) {
+                array_pop($I['w']);
+                $adult_count--;
+            }
+
+            $fee = $fee + $base_fee_qtr * count($I['w']);
+        }
+
+        return $fee;
+    }
+
+    public function clear()
+    {
+        unset($this->data, $this->riders);
+    }
+
+
+
+    public function getBaseFee()
+    {
+        return $this->base_fee;
     }
 }
--- tests/priceCalcTest.php
+++ tests/priceCalcTest.php
@@ -24,7 +24,16 @@ class priceCalcTest extends \PHPUnit_Framework_TestCase
 
     public function testDispatch()
     {
+        $data_collection = $this->getTestData();
+        $priceCalc = $this->SUT;
 
+        foreach($data_collection as $data) {
+            $ret = $priceCalc->dispatch($data[0]);
+            $this->assertEquals($ret, $data[1], sprintf('%s  → %s', $data[0], $data[1]) );
+            // echo sprintf('%s  → %s', $ret, $data[1]) . PHP_EOL;
+
+            $priceCalc->clear();
+        }
     }
 
 
@@ -33,8 +42,57 @@ class priceCalcTest extends \PHPUnit_Framework_TestCase
         $priceCalc = $this->SUT;
 
         $priceCalc->ParseData('210:Cn,In,Iw,Ap,Iw');
-        $ret = $priceCalc->getBasicFee();
+        $ret = $priceCalc->getBaseFee();
 
         $this->assertEquals('210', $ret);
+    }
+
+    private function getTestData()
+    {
+        $data = [
+            ["210:Cn,In,Iw,Ap,Iw", "170"],
+            ["220:Cp,In", "110"],
+            ["230:Cw,In,Iw", "240"],
+            ["240:In,An,In", "240"],
+            ["250:In,In,Aw,In", "260"],
+            ["260:In,In,In,In,Ap", "260"],
+            ["270:In,An,In,In,Ip", "410"],
+            ["280:Aw,In,Iw,In", "210"],
+            ["200:An", "200"],
+            ["210:Iw", "60"],
+            ["220:Ap", "0"],
+            ["230:Cp", "0"],
+            ["240:Cw", "60"],
+            ["250:In", "130"],
+            ["260:Cn", "130"],
+            ["270:Ip", "0"],
+            ["280:Aw", "140"],
+            ["1480:In,An,In,In,In,Iw,Cp,Cw,In,Aw,In,In,Iw,Cn,Aw,Iw", "5920"],
+            ["630:Aw,Cw,Iw,An,An", "1740"],
+            ["340:Cn,Cn,Ip,Ap", "340"],
+            ["240:Iw,Ap,In,Iw,Aw", "120"],
+            ["800:Cw,An,Cn,Aw,Ap", "1800"],
+            ["1210:An,Ip,In,Iw,An,Iw,Iw,An,Iw,Iw", "3630"],
+            ["530:An,Cw,Cw", "810"],
+            ["170:Aw,Iw,Ip", "90"],
+            ["150:In,Ip,Ip,Iw,In,Iw,Iw,In,An,Iw,Aw,Cw,Iw,Cw,An,Cp,Iw", "580"],
+            ["420:Cn,Cw,Cp", "320"],
+            ["690:Cw,In,An,Cp,Cn,In", "1220"],
+            ["590:Iw,Iw,Cn,Iw,Aw,In,In,Ip,Iw,Ip,Aw", "1200"],
+            ["790:Cw,Cn,Cn", "1000"],
+            ["1220:In,In,An,An,In,Iw,Iw,In,In,Ip,In,An,Iw", "4590"],
+            ["570:Cw,Cn,Cp", "440"],
+            ["310:Cn,Cw,An,An,Iw,Cp,Cw,Cn,Iw", "1100"],
+            ["910:Aw,In,Iw,Iw,Iw,Iw,Iw,An,Cw,In", "2290"],
+            ["460:Iw,Cw,Cw,Cn", "590"],
+            ["240:Iw,Iw,In,Iw,In,In,Cn,In,An", "780"],
+            ["1240:In,In,In,Ap,In,Cw,Iw,Iw,Iw,Aw,Cw", "2170"],
+            ["1000:Iw,Ip,In,An,In,In,In,An,In,Iw,In,In,Iw,In,Iw,Iw,Iw,An", "5500"],
+            ["180:In,Aw,Ip,Iw,In,Aw,In,Iw,Iw,In", "330"],
+            ["440:In,Ip,Cp,Aw,Iw,In,An", "660"],
+            ["1270:Ap,In,An,Ip,In,Ip,Ip", "1270"]
+        ];
+
+        return $data;
     }
 }
